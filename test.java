15a02a503031fce5955b6aad9ed3d5b695546616
Irina Anton <antony.ira1991@gmail.com>
2023-02-26 16:40:00 -0600

    I finish my work

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
 public class test{
 
 public static void main(String[] args){
-       System.out.println("Changes From SDET-2");
+       System.out.println("Changes From dev2");
+System.out.println("More changes from dev2");
 
 }
 
